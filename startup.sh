65141484aa494e810471480168c9e5b9a47b0573
AlishbaFatima-84 <alishbafatima84@gmail.com>
2025-04-24 18:36:03 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,14 @@
-#!/bin/bash
-gunicorn app:app --timeout 300 --workers 1 --bind=0.0.0.0:$PORT
+cat > startup.sh << 'EOF'
+#!/bin/sh
+echo "🔁 Creating or activating virtualenv"
+if [ ! -d "antenv" ]; then
+    python -m venv antenv
+fi
+. antenv/bin/activate
+pip install -r requirements.txt
+
+echo "🚀 Launching app via Gunicorn"
+exec gunicorn --bind=0.0.0.0:${PORT:-8000} app:app
+EOF
+
+chmod +x startup.sh
